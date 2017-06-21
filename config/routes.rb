00fcf4162bc5ff67f3434f5e47b819f99ce30b2b
Samantha Cooks <samtcooks@gmail.com>
2017-06-21 14:23:25 -0400

    Final update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :authors
-  root to: 'blog/posts#index'
+  # devise_for :authors
+  # root to: 'blog/posts#index'
 
-  namespace :authors do
-    resources :posts
+  # namespace :authors do
+  #   resources :posts
+  # end
+
+  # scope module: 'blog' do
+  #   get 'about' => 'pages#about', as: :about
+  #   get 'contact' => 'pages#contact', as: :contact
+  #   get 'posts' => '_posts#index', as: :posts
+  #   get 'post/:id' => 'posts#show', as: :post
+  # end
+  resources :experiment do 
+    resources :observation
+    resources :comment
   end
-
-  scope module: 'blog' do
-    get 'about' => 'pages#about', as: :about
-    get 'contact' => 'pages#contact', as: :contact
-    get 'posts' => '_posts#index', as: :posts
-    get 'post/:id' => 'posts#show', as: :post
-  end
-
 end
