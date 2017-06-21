a288d63aba812e5914d49859871fcb764efdfc49
Samantha Cooks <samtcooks@gmail.com>
2017-06-21 03:49:28 -0400

    Add motion ui features

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  $(".button").click(function() {
+
+    var $animation = $("#boom").data("animation");
+
+    MotionUI.animateIn($("#boom"), $animation);
+
+  });
+});
